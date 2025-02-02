31d69b1d57582555ad8a1b5cdd2c1927392a3214
Fabio Lopes <fabiolopes.dev@gmail.com>
2025-02-02 13:06:01 -0300

    Inserindo modelo estático de movimento

--- edge_sim_py/components/mobility_models/__init__.py
+++ edge_sim_py/components/mobility_models/__init__.py
@@ -6,3 +6,4 @@ from .pathway import pathway
 from .random_mobility import random_mobility
 from .random_waypoint_mobility import random_waypoint_mobility
 from .retangle_mobility import retangle_mobility
+from .static import static
